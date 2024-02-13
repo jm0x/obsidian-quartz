a5fac68e8154f38f03214d3486eb994284beb753
Juan Márquez <jm0x@proton.me>
2024-02-13 13:01:10 +0100

    Quartz sync: Feb 13, 2024, 1:01 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,7 +3,7 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 My blog about AI",
+    pageTitle: "🪴 SomaDL",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
